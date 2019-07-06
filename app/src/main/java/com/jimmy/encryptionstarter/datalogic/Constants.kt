6b37ac3525af61b1765d450dd49caea860e5abaf
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2019-07-06 12:44:35 +0200

    - add encryption ref. constants

--- app/src/main/java/com/jimmy/encryptionstarter/datalogic/Constants.kt
+++ app/src/main/java/com/jimmy/encryptionstarter/datalogic/Constants.kt
@@ -2,4 +2,7 @@ package com.jimmy.encryptionstarter.datalogic
 
 object FileConstants {
     const val DATA_SOURCE_FILE_NAME = "pets.xml"
+    const val ENCRYPTION_ALGORITHM = "PBKDF2WithHmacSHA1"
+    const val AES = "AES"
+    const val TRANSFORMATION = "AES/CBC/PKCS7Padding"
 }
