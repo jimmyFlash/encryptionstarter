3e2115eaeb94618a1d2b697390c31a6b8cccaae0
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2019-07-14 18:43:13 +0200

    - handle BadPaddingException when entering wrong password - use kotlin dsl to move dependencies (WIP)

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins{
+//    plugin allows your to use kotlin for gradle build scripts
+    `kotlin-dsl`
+}
+
+repositories {
+    jcenter()
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,9 @@
+object Version{
+
+    val kotlinVersion = "1.3.11"
+
+}
+
+object Deps{
+    val kotlin_sdkLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.kotlinVersion}"
+}
